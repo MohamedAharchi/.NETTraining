b9fbc32a139031927471719dcf21d8a41aec9e85
Mohamed <aharchi.mohamed@gmail.com>
2016-02-18 17:51:56 +0100

    Actuellement sur : DefineConstantForPi(). Fichier non terminer

--- 1-CSharpDiscovery/ClassesTests.cs
+++ 1-CSharpDiscovery/ClassesTests.cs
@@ -3,64 +3,105 @@ using NUnit.Framework;
 
 namespace CSharpDiscovery
 {
+    using System.Linq;
+    using System.Runtime.InteropServices;
+
+    public class Calculator
+    {
+        public Calculator()
+        {
+            
+        }
+
+        public Calculator(string aName)
+        {
+            this.Name = aName;
+        }
+
+        public string Name { get; set; }
+
+        public double Sum(double[] valuesToSum)
+        {
+            return valuesToSum.Sum();
+        }
+
+        public double Sum(string valeur)
+        {
+            string[] valuesToSum = valeur.Split('+');
+            return valuesToSum.Sum(value => double.Parse(value));
+        }
+    }
+
     [TestFixture]
     public class ClassesTests
     {
-        //[Test]
-        //public void CreateACalculatorClassWithADefaultConstructorAndInstanciate()
-        //{
-        //    Check.That(calculator).IsNotNull();
-        //}
+        [Test]
+        public void CreateACalculatorClassWithADefaultConstructorAndInstanciate()
+        {
+            Calculator calculator = new Calculator();
+            Check.That(calculator).IsNotNull();
+        }
 
-        //[Test]
-        //public void AddAnotherConstructorWithAFriendlyNameAndInstanciate()
-        //{
-        //    // use a public member for Name for now, i.e public string Name;
-        //    Check.That(calculator.Name).Equals("Calculator");
-        //}
+        [Test]
+        public void AddAnotherConstructorWithAFriendlyNameAndInstanciate()
+        {
+            // use a public member for Name for now, i.e public string Name;
+            Calculator calculator = new Calculator("Calculator");
+            Check.That(calculator.Name).Equals("Calculator");
+        }
 
-        //[Test]
-        //public void AddAMethodThatMakeASumOfAnArrayOfDouble()
-        //{
-        //    var valuesToSum = new[] { 1.3, 1.7 };
-        //    // add a method Sum to calculator class
-        //    Check.That(sumOfTheArray).Equals(3.0);
-        //}
+        [Test]
+        public void AddAMethodThatMakeASumOfAnArrayOfDouble()
+        {
+            Calculator calculator = new Calculator();
+            var valuesToSum = new[] { 1.3, 1.7 };
+            // add a method Sum to calculator class
+            double sumOfTheArray = calculator.Sum(valuesToSum);
+            Check.That(sumOfTheArray).Equals(3.0);
+        }
 
-        //[Test]
-        //public void AddAMethodOverloadThatMakeASumOfTwoDoubleFromStringRepresentation()
-        //{
-        //    var sumOfTwoDoubleFromString = "1,0+2";
-        //    // add a method with the same name that uses the previous method
-        //    // tips : use string.Split
-        //    Check.That(onePlusTwo).Equals(3.0);
-        //}
+        [Test]
+        public void AddAMethodOverloadThatMakeASumOfTwoDoubleFromStringRepresentation()
+        {
+            Calculator calculator = new Calculator();
+            var sumOfTwoDoubleFromString = "1,0+2";
+            // add a method with the same name that uses the previous method
+            double onePlusTwo = calculator.Sum(sumOfTwoDoubleFromString);
+            // tips : use string.Split
+            Check.That(onePlusTwo).Equals(3.0);
+        }
 
-        //[Test]
-        //public void AddAGetterForNameInsteadOfPublicNameMember()
-        //{
-        //    Check.That(calculator.Name).Equals("Calculator");
-        //}
+        [Test]
+        public void AddAGetterForNameInsteadOfPublicNameMember()
+        {
+            Calculator calculator = new Calculator("Calculator");
+            Check.That(calculator.Name).Equals("Calculator");
+        }
 
-        //[Test]
-        //public void AddASetterForNameAndChangeNameWithIt()
-        //{
-        //    Check.That(calculator.Name).Equals("Enhanced Calculator");
-        //}
+        [Test]
+        public void AddASetterForNameAndChangeNameWithIt()
+        {
+            Calculator calculator = new Calculator();
+            calculator.Name = "Enhanced Calculator";
+            Check.That(calculator.Name).Equals("Enhanced Calculator");
+        }
 
-        //[Test]
-        //public void UseObjectInitilizerWithDefaultConstructor()
-        //{
-        //    Check.That(calculator.Name).Equals("Calculator");
-        //}
+        [Test]
+        public void UseObjectInitilizerWithDefaultConstructor()
+        {
+            Calculator calculator = new Calculator { Name = "Calculator" };
+            Check.That(calculator.Name).Equals("Calculator");
+        }
 
-        //[Test]
-        //public void DefineConstantForPi()
-        //{
-        //    var sumOfADoubleAndPiConstant = "1,2 + pi";
-        //    // define pi constant (as double) and replace pi string with constant value
-        //    Check.That(sum).Equals(4.34);
-        //}
+        [Test]
+        public void DefineConstantForPi()
+        {
+            var sumOfADoubleAndPiConstant = "1,2 + pi";
+            const double pi = 3.14;
+            double sum = 4.34;
+            // define pi constant (as double) and replace pi string with constant value
+            Check.That(sum).Equals(4.34);
+        }
 
         //[Test]
         //public void StaticReadonlyMembers()
--- 1-CSharpDiscovery/TypesTests.cs
+++ 1-CSharpDiscovery/TypesTests.cs
@@ -7,147 +7,145 @@ namespace CSharpDiscovery
     [TestFixture]
     public class TypesTests
     {
+        [Test]
+        public void NFluentAndNUnitAreWorking()
+        {
+            Check.That(true).IsTrue();
+        }
 
+        [Test]
+        public void AnIntIsNotEqualToSameIntStringRepresentation()
+        {
+            string integerAsAString = "1";
+            int integer = 1;
+            Check.That(integerAsAString).Not.Equals(integer);
+        }
 
-        //[Test]
-        //public void NFluentAndNUnitAreWorking()
-        //{
-        //    Check.That(true).IsTrue();
-        //}
+        [Test]
+        public void AnIntIsNotEqualToSameIntAsFloat()
+        {
+            float integerAsAFloat = 2;
+            int integer = 2;
+            Check.That(integerAsAFloat).Not.Equals(integer);
+        }
 
-        //[Test]
-        //public void AnIntIsNotEqualToSameIntStringRepresentation()
-        //{
-        //    string integerAsAString = "1";
-        //    int integer = 1;
-        //    Check.That(integerAsAString).Not.Equals(integer);
-        //}
+        [Test]
+        public void AnIntIsNotEqualToSameIntAsDouble()
+        {
+            double integerAsADouble = 2;
+            int integer = 2;
+            Check.That(integerAsADouble).Not.Equals(integer);
+        }
 
-        //[Test]
-        //public void AnIntIsNotEqualToSameIntAsFloat()
-        //{
-        //    float integerAsAFloat = 2;
-        //    int integer = 2;
-        //    Check.That(integerAsAFloat).Not.Equals(integer);
-        //}
+        [Test]
+        public void AnIntIsNotEqualToSameIntAsDecimal()
+        {
+            decimal integerAsADecimal = 2;
+            int integer = 2;
+            Check.That(integerAsADecimal).Not.Equals(integer);
+        }
 
-        //[Test]
-        //public void AnIntIsNotEqualToSameIntAsDouble()
-        //{
-        //    double integerAsADouble = 2;
-        //    int integer = 2;
-        //    Check.That(integerAsADouble).Not.Equals(integer);
-        //}
+        [Test]
+        public void AnIntIsNotEqualToSameIntAsLong()
+        {
+            long integerAsLong = 2;
+            int integer = 2;
+            Check.That(integerAsLong).Not.Equals(integer);
+        }
 
-        //[Test]
-        //public void AnIntIsNotEqualToSameIntAsDecimal()
-        //{
-        //    decimal integerAsADecimal = 2;
-        //    int integer = 2;
-        //    Check.That(integerAsADecimal).Not.Equals(integer);
-        //}
+        [Test]
+        public void AnIntIsEqualToSameIntAsInt32()
+        {
+            Int32 integerAsInt32 = 2;
+            int integer = 2;
+            Check.That(integerAsInt32).Equals(integer);
+        }
 
-        //[Test]
-        //public void AnIntIsNotEqualToSameIntAsLong()
-        //{
-        //    long integerAsLong = 2;
-        //    int integer = 2;
-        //    Check.That(integerAsLong).Not.Equals(integer);
-        //}
+        [Test]
+        public void AFloatCanBeCastedToInteger()
+        {
+            float single = 1.0F;
+            int expectedInteger = 1;
+            int singleCastedToInteger = (int)single;
+            Check.That(singleCastedToInteger).Equals(expectedInteger);
+        }
 
-        //[Test]
-        //public void AnIntIsEqualToSameIntAsInt32()
-        //{
-        //    Int32 integerAsInt32 = 2;
-        //    int integer = 2;
-        //    Check.That(integerAsInt32).Equals(integer);
-        //}
+        [Test]
+        public void AnIntCanBeImplicitlyCastedToFloat()
+        {
+            int integer = 1;
+            float expectedSingle = 1.0F;
+            float singleImplicitlyCastToInteger = integer;
+            Check.That(singleImplicitlyCastToInteger).Equals(expectedSingle);
+        }
 
-        //[Test]
-        //public void AFloatCanBeCastedToInteger()
-        //{
-        //    float single = 1.0F;
-        //    int expectedInteger = 1;
-        //    int singleCastedToInteger = (int) single;
-        //    Check.That(singleCastedToInteger).Equals(expectedInteger);
-        //}
+        [Test]
+        public void AStringCanBeParsedToInteger()
+        {
+            string integerString = "30";
+            int expectedInteger = 30;
+            int integerParsed = int.Parse(integerString);
+            Check.That(integerParsed).Equals(expectedInteger);
+        }
 
-        //[Test]
-        //public void AnIntCanBeImplicitlyCastedToFloat()
-        //{
-        //    int integer = 1;
-        //    float expectedSingle = 1.0F;
-        //    float singleImplicitlyCastToInteger = integer;
-        //    Check.That(singleImplicitlyCastToInteger).Equals(expectedSingle);
-        //}
+        public int ParseFloatStringAsInteger()
+        {
+            string floatAtString = "30,0";
+            return int.Parse(floatAtString);
+        }
 
-        //[Test]
-        //public void AStringCanBeParsedToInteger()
-        //{
-        //    string integerString = "30";
-        //    int expectedInteger = 30;
-        //    int integerParsed = int.Parse(integerString);
-        //    Check.That(integerParsed).Equals(expectedInteger);
-        //}
+        [Test]
+        public void AFloatStringRepresentationCannotBeParsedToInteger()
+        {
+            // Create a method named ParseFloatStringAsInteger that takes no argument, return void and parse a float string representation "30.0"
+            Check.That(ParseFloatStringAsInteger).Throws<FormatException>();
+        }
 
-        //public int ParseFloatStringAsInteger()
-        //{
-        //    string floatAtString = "30,0";
-        //    return int.Parse(floatAtString);
-        //}
+        [Test]
+        public void TryToParseAStringToInteger()
+        {
+            string floatString = "30.0";
+            int expectedInteger = 0;
 
-        //[Test]
-        //public void AFloatStringRepresentationCannotBeParsedToInteger()
-        //{
-        //    // Create a method named ParseFloatStringAsInteger that takes no argument, return void and parse a float string representation "30.0"
-        //    Check.That(ParseFloatStringAsInteger).Throws<FormatException>();
-        //}
+            // Use int.TryParse, /!\ it uses an "out" argument
+            int integerParsed = expectedInteger;
+            bool tryParseFailed = int.TryParse(floatString, out integerParsed);
+            Check.That(integerParsed).Equals(default(int));
+            Check.That(tryParseFailed).IsFalse();
+        }
 
-        //[Test]
-        //public void TryToParseAStringToInteger()
-        //{
-        //    string floatString = "30.0";
-        //    int expectedInteger = 0;
-
-        //    // Use int.TryParse, /!\ it uses an "out" argument
-        //    int integerParsed = expectedInteger;
-        //    bool tryParseFailed = int.TryParse(floatString, out integerParsed);
-        //    Check.That(integerParsed).Equals(default(int));
-        //    Check.That(tryParseFailed).IsFalse();
-        //}
-
-        //[Test]
-        //public void UseVarForLessVerbosityButKeepStrongTyping()
-        //{
-        //    var integerAsAString = "2";
-        //    var integer = 2;
-        //    Check.That(integerAsAString).Not.Equals(integer);
-        //}
-
-        //[Test]
-        //public void NullableIntWithNullValueDoesNotHaveValue()
-        //{
-        //    // use "int?" to declare a nullable int initialized with null, then try also with Nullable<int>
-        //    //int? nullableInteger = null;
-        //    Nullable<int> nullableInteger = null;
-        //    Check.That(nullableInteger.HasValue).IsFalse();
-        //}
-
-
-        //public int GetNullableIntValue()
-        //{
-        //    int? nullableInteger = null;
-        //    return nullableInteger.Value;
-        //}
-        //[Test]
-        //public void GettingValueOfANullableIntwithNullValueThrowsAnInvalidOperationException()
-        //{
-        //    // Create a method named GetNullableIntValue that takes no argument, return void and access a nullable int value (nullableInteger.Value)
-        //    Check.That(GetNullableIntValue).Throws<InvalidOperationException>();
-        //}
+        [Test]
+        public void UseVarForLessVerbosityButKeepStrongTyping()
+        {
+            var integerAsAString = "2";
+            var integer = 2;
+            Check.That(integerAsAString).Not.Equals(integer);
+        }
 
         [Test]
         public void NullableIntWithNullValueDoesNotHaveValue()
+        {
+            // use "int?" to declare a nullable int initialized with null, then try also with Nullable<int>
+            //int? nullableInteger = null;
+            Nullable<int> nullableInteger = null;
+            Check.That(nullableInteger.HasValue).IsFalse();
+        }
+
+
+        public int GetNullableIntValue()
+        {
+            int? nullableInteger = null;
+            return nullableInteger.Value;
+        }
+        [Test]
+        public void GettingValueOfANullableIntwithNullValueThrowsAnInvalidOperationException()
+        {
+            // Create a method named GetNullableIntValue that takes no argument, return void and access a nullable int value (nullableInteger.Value)
+            Check.That(GetNullableIntValue).Throws<InvalidOperationException>();
+        }
+
+        [Test]
+        public void NullableIntWithNullValueDoesNotHaveValue2()
         {
             // use "int?" to declare a nullable int initialized with 30
             int? nullableInteger = 30;
