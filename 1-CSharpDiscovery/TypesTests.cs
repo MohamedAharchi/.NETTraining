553b6a2f6780ee2ef73c4d9bf3938e17197ff80a
Mohamed <aharchi.mohamed@gmail.com>
2016-02-18 16:56:01 +0100

    TypesTests.cs terminer

--- 1-CSharpDiscovery/TypesTests.cs
+++ 1-CSharpDiscovery/TypesTests.cs
@@ -7,6 +7,8 @@ namespace CSharpDiscovery
     [TestFixture]
     public class TypesTests
     {
+
+
         //[Test]
         //public void NFluentAndNUnitAreWorking()
         //{
@@ -16,36 +18,48 @@ namespace CSharpDiscovery
         //[Test]
         //public void AnIntIsNotEqualToSameIntStringRepresentation()
         //{
+        //    string integerAsAString = "1";
+        //    int integer = 1;
         //    Check.That(integerAsAString).Not.Equals(integer);
         //}
 
         //[Test]
         //public void AnIntIsNotEqualToSameIntAsFloat()
         //{
+        //    float integerAsAFloat = 2;
+        //    int integer = 2;
         //    Check.That(integerAsAFloat).Not.Equals(integer);
         //}
 
         //[Test]
         //public void AnIntIsNotEqualToSameIntAsDouble()
         //{
+        //    double integerAsADouble = 2;
+        //    int integer = 2;
         //    Check.That(integerAsADouble).Not.Equals(integer);
         //}
 
         //[Test]
         //public void AnIntIsNotEqualToSameIntAsDecimal()
         //{
+        //    decimal integerAsADecimal = 2;
+        //    int integer = 2;
         //    Check.That(integerAsADecimal).Not.Equals(integer);
         //}
 
         //[Test]
         //public void AnIntIsNotEqualToSameIntAsLong()
         //{
+        //    long integerAsLong = 2;
+        //    int integer = 2;
         //    Check.That(integerAsLong).Not.Equals(integer);
         //}
 
         //[Test]
         //public void AnIntIsEqualToSameIntAsInt32()
         //{
+        //    Int32 integerAsInt32 = 2;
+        //    int integer = 2;
         //    Check.That(integerAsInt32).Equals(integer);
         //}
 
@@ -54,7 +68,7 @@ namespace CSharpDiscovery
         //{
         //    float single = 1.0F;
         //    int expectedInteger = 1;
-
+        //    int singleCastedToInteger = (int) single;
         //    Check.That(singleCastedToInteger).Equals(expectedInteger);
         //}
 
@@ -63,7 +77,7 @@ namespace CSharpDiscovery
         //{
         //    int integer = 1;
         //    float expectedSingle = 1.0F;
-
+        //    float singleImplicitlyCastToInteger = integer;
         //    Check.That(singleImplicitlyCastToInteger).Equals(expectedSingle);
         //}
 
@@ -72,8 +86,14 @@ namespace CSharpDiscovery
         //{
         //    string integerString = "30";
         //    int expectedInteger = 30;
-            
+        //    int integerParsed = int.Parse(integerString);
         //    Check.That(integerParsed).Equals(expectedInteger);
+        //}
+
+        //public int ParseFloatStringAsInteger()
+        //{
+        //    string floatAtString = "30,0";
+        //    return int.Parse(floatAtString);
         //}
 
         //[Test]
@@ -90,7 +110,8 @@ namespace CSharpDiscovery
         //    int expectedInteger = 0;
 
         //    // Use int.TryParse, /!\ it uses an "out" argument
-
+        //    int integerParsed = expectedInteger;
+        //    bool tryParseFailed = int.TryParse(floatString, out integerParsed);
         //    Check.That(integerParsed).Equals(default(int));
         //    Check.That(tryParseFailed).IsFalse();
         //}
@@ -98,6 +119,8 @@ namespace CSharpDiscovery
         //[Test]
         //public void UseVarForLessVerbosityButKeepStrongTyping()
         //{
+        //    var integerAsAString = "2";
+        //    var integer = 2;
         //    Check.That(integerAsAString).Not.Equals(integer);
         //}
 
@@ -105,10 +128,17 @@ namespace CSharpDiscovery
         //public void NullableIntWithNullValueDoesNotHaveValue()
         //{
         //    // use "int?" to declare a nullable int initialized with null, then try also with Nullable<int>
-            
+        //    //int? nullableInteger = null;
+        //    Nullable<int> nullableInteger = null;
         //    Check.That(nullableInteger.HasValue).IsFalse();
         //}
 
+
+        //public int GetNullableIntValue()
+        //{
+        //    int? nullableInteger = null;
+        //    return nullableInteger.Value;
+        //}
         //[Test]
         //public void GettingValueOfANullableIntwithNullValueThrowsAnInvalidOperationException()
         //{
@@ -116,12 +146,12 @@ namespace CSharpDiscovery
         //    Check.That(GetNullableIntValue).Throws<InvalidOperationException>();
         //}
 
-        //[Test]
-        //public void NullableIntWithNullValueDoesNotHaveValue()
-        //{
-        //    // use "int?" to declare a nullable int initialized with 30
-            
-        //    Check.That(nullableInteger.Value).Equals(30);
-        //}
+        [Test]
+        public void NullableIntWithNullValueDoesNotHaveValue()
+        {
+            // use "int?" to declare a nullable int initialized with 30
+            int? nullableInteger = 30;
+            Check.That(nullableInteger.Value).Equals(30);
+        }
     }
 }
